6062e97d35bff49b76687ea7a728fd7ae1685730
psanjuan <p.sanjuan@upv.es>
2020-07-09 00:53:55 +0200

    Added multiple precission support

--- compGEMM.c
+++ compGEMM.c
@@ -16,25 +16,46 @@
 
 
 
-#define EPS 1e10
 
 
+#ifdef fp_D
+    #define fpType double
+    #define EPS 1e-16
+#elif fp_H
+    #define fpType _Float16
+    #define EPS 5e-4
+#elif fp_HS
+    #define fpType _Float16
+    #define fp_H 1
+    #define EPS 5e-4
+#else
+    #define fpType float
+    #define EPS 6e-8
+#endif
+
+double compareMatrix(int m, int n, fpType *M, int ldm, fpType *M2, int ldm2 );
+int print_matrix( char *name, int m, int n, fpType *M, int ldm );
+int print_matrices( int m, int n, char *name, fpType *M, int ldm,  char *name2, fpType *M2, int ldm2);
 
 int main( int argc, char** argv )
 {
     double tBlis, tOwn, tIni,
             gflopsBlis, gflopsOwn;
             
-    float ONE = 1, ZERO = 0;
+    fpType ONE = 1, ZERO = 0;
     
     int i,
         m, n, k, //matrix dimms
         repe;
     
-    float *A, *B, *CBlis, *COwn,
+    fpType *A, *B, *CBlis, *COwn,
             *Ac_pack, *Bc_pack;
+#ifdef fp_H
+    float* Afloat, *Bfloat, *Cfloat; 
+    float fONE = 1, fZERO = 0;
+#endif
 
-    float norm = 0, normOrig = 0;
+    fpType norm = 0, normOrig = 0;
     
     if (argc != 5)
     {
@@ -50,44 +71,110 @@ int main( int argc, char** argv )
     k = atoi(argv[3]);
     repe  =atoi(argv[4]);
     
-    A = (float*) malloc(m*k * sizeof(float));
-    B = (float*) malloc(k*n * sizeof(float));
-    CBlis = (float*) malloc(m*n * sizeof(float));
-    COwn = (float*) malloc(m*n * sizeof(float));
+    A = (fpType*) malloc(m*k * sizeof(fpType));
+    B = (fpType*) malloc(k*n * sizeof(fpType));
+    CBlis = (fpType*) malloc(m*n * sizeof(fpType));
+    COwn = (fpType*) malloc(m*n * sizeof(fpType));
+        //print_matrix("CBuff",m,n,COwn,m);
+        //print_matrix("CBuff1",m,n,COwn,m);
+#ifdef fp_H
+    Afloat = (float*) malloc(m*k * sizeof(float));
+    Bfloat = (float*) malloc(k*n * sizeof(float));
+    Cfloat = (float*) malloc(m*n * sizeof(float));
+#endif
     
-    Ac_pack = (float*) aligned_alloc(4096,BLOCK_MC*BLOCK_KC*sizeof(float));
-    Bc_pack = (float*) aligned_alloc(4096,BLOCK_KC*BLOCK_NC*sizeof(float));
+    Ac_pack = (fpType*) aligned_alloc(4096,BLOCK_MC*BLOCK_KC*sizeof(fpType));
+    Bc_pack = (fpType*) aligned_alloc(4096,BLOCK_KC*BLOCK_NC*sizeof(fpType));
     
-    
+
+#ifdef fp_D
+    bli_drandm( 0, BLIS_DENSE, m, k, A, 1, m );
+    bli_drandm( 0, BLIS_DENSE, k, n, B, 1, k );
+#elif fp_H
+   //print_matrix("CBuff",m,n,COwn,m);
+    bli_srandm( 0, BLIS_DENSE, m, k, Afloat, 1, m );
+    decreasePrecissionV_SH(m*k,Afloat,A);
+    bli_srandm( 0, BLIS_DENSE, k, n, Bfloat, 1, k );
+    decreasePrecissionV_SH(k*n,Bfloat,B);    
+#else
     bli_srandm( 0, BLIS_DENSE, m, k, A, 1, m );
     bli_srandm( 0, BLIS_DENSE, k, n, B, 1, k );
+#endif
 
+
+    //Timing gemm blis
+    tIni = bli_clock();
     for(i = 0; i <  repe; i++)
     {
-        //Timing gemm blis
-        tIni = bli_clock();
+#ifdef fp_D
+        bli_dgemm(BLIS_NO_TRANSPOSE,BLIS_NO_TRANSPOSE,m,n,k,&ONE,A,1,m,B,1,k,&ZERO,CBlis,1,m);
+#elif fp_H
+        increasePrecissionV_HS(m*k,A,Afloat);
+        increasePrecissionV_HS(k*n,B,Bfloat);
+        bli_sgemm(BLIS_NO_TRANSPOSE,BLIS_NO_TRANSPOSE,m,n,k,&fONE,Afloat,1,m,Bfloat,1,k,&fZERO,Cfloat,1,m); 
+        decreasePrecissionV_SH(m*n,Cfloat,CBlis);
+#else
         bli_sgemm(BLIS_NO_TRANSPOSE,BLIS_NO_TRANSPOSE,m,n,k,&ONE,A,1,m,B,1,k,&ZERO,CBlis,1,m);
-        tBlis += bli_clock() - tIni;
-        
-        //Timing custom gemm 
-        tIni = bli_clock();
+#endif
+    }
+    tBlis = bli_clock() - tIni;
+    
+    
+    //Timing custom gemm 
+    tIni = bli_clock();
+    for(i = 0; i <  repe; i++)
+    {
+#ifdef fp_D
+        dgemm_cust(m,n,k,1.0,A,m,B,k,0.0,COwn,m,Ac_pack,Bc_pack);
+#elif fp_HS
+        hsgemm_cust(m,n,k,1.0,A,m,B,k,0.0,COwn,m,Ac_pack,Bc_pack, Cfloat);
+#elif fp_H
+        hgemm_cust(m,n,k,1.0,A,m,B,k,0.0,COwn,m,Ac_pack,Bc_pack);
+#else
         sgemm_cust(m,n,k,1.0,A,m,B,k,0.0,COwn,m,Ac_pack,Bc_pack);
-        tOwn += bli_clock() -tIni;
+#endif
+    }
+    tOwn = bli_clock() -tIni;
         
                 
 #ifdef COMPARE
-         bli_snormfv(m*n,COwn,1,&normOrig);
-         bli_ssubm(0,BLIS_NONUNIT_DIAG,BLIS_DENSE,BLIS_NO_TRANSPOSE,m,n,CBlis,1,m,COwn,1,m);
-         bli_snormfv(m*n,COwn,1,&norm);
+  #ifdef fp_D
+         bli_dnormfv(m*n,COwn,1,&normOrig);
+         bli_dsubm(0,BLIS_NONUNIT_DIAG,BLIS_DENSE,BLIS_NO_TRANSPOSE,m,n,CBlis,1,m,COwn,1,m);
+         bli_dnormfv(m*n,COwn,1,&norm);
          printf("Approximation error: %g\n",norm/normOrig);
          if (norm > EPS)
          {
              printf("Error threshold exceeded\n");
              exit(1);
          }
+  #elif fp_H
+        norm = compareMatrix(m,n,COwn, m,CBlis,m);
+        //print_matrices( m, n,  "Own", COwn, m, "Blis", CBlis, m);
+      
+        
+        printf("Approximation error: %g\n",(double)norm);
+         if (norm > EPS)
+         {
+             printf("Error threshold exceeded\n");
+            // exit(1);
+         }
+  #else
+         bli_snormfv(m*n,COwn,1,&normOrig);
+         norm = compareMatrix(m,n,COwn, m,CBlis,m);
+         printf("Approximation erro compareMatrix: %g\n",norm);
+         bli_ssubm(0,BLIS_NONUNIT_DIAG,BLIS_DENSE,BLIS_NO_TRANSPOSE,m,n,CBlis,1,m,COwn,1,m);
+         bli_snormfv(m*n,COwn,1,&norm);
          
+         printf("Approximation error: %g\n",norm/normOrig);
+         if (norm > EPS)
+         {
+             printf("Error threshold exceeded\n");
+             exit(1);
+         }
+  #endif
 #endif
-    }
+    
     
     tBlis /=repe;
     tOwn/=repe;
@@ -95,6 +182,16 @@ int main( int argc, char** argv )
     gflopsBlis = ( 2.0 * m * k * n ) / ( tBlis * 1.0e9 );
     gflopsOwn = ( 2.0 * m * k * n ) / ( tOwn * 1.0e9 );
     
+#ifdef fp_D
+        printf("Precision: double\n");
+#elif fp_HS
+        printf("Precision: half with simple precission accumulation\n");
+#elif fp_H
+        printf("Precision: half\n");
+#else
+        printf("Precision: simple\n");
+#endif
+        
     printf("BLIS Time: %.3f GFlops: %.3f\n",tBlis,gflopsBlis);
     printf("Custom Time: %.3f GFlops: %.3f\n",tOwn,gflopsOwn);
     
@@ -104,6 +201,23 @@ int main( int argc, char** argv )
     free(CBlis);
     free(COwn);
     
+}
+
+double compareMatrix(int m, int n, fpType *M, int ldm, fpType *M2, int ldm2 )
+{
+    int i,j;
+    double Aux = 0;
+    double Aux2 = 0;
+    
+    
+    for(j=0; j < n; j++)
+        for(i=0; i < m; i++)
+        {
+            Aux += pow(M[i+j*ldm] - M2[i + j* ldm2],2);
+            Aux2+= M[i+j*ldm] * M[i+j*ldm];
+            
+        }
+    return sqrt(Aux)/sqrt(Aux2);
 }
 
 
@@ -117,15 +231,26 @@ int main( int argc, char** argv )
  * @param[in] Alda Leading dimension
  *
  */
-int print_matrix( char *name, int m, int n, float *A, int Alda )
+int print_matrix( char *name, int m, int n, fpType *M, int ldm )
 {
 
   int i, j;
 
-  for ( j=1; j<=n; j++ )
-    for ( i=1; i<=m; i++ )
-      printf( "   %s(%d,%d) = %22.15e;\n", name, i, j, A[m*(j-1)+(i-1)]);
+  for ( j=0; j<n; j++ )
+    for ( i=0; i<m; i++ )
+      printf( "   %s(%d,%d) = %22.15e;\n", name, i, j, (double)M[i +j * ldm]);
 
   return 0;
 }
 
+int print_matrices( int m, int n, char *name, fpType *M, int ldm,  char *name2, fpType *M2, int ldm2)
+{
+
+  int i, j;
+
+  for ( j=0; j<n; j++ )
+    for ( i=0; i<m; i++ )
+      printf( "   (%d,%d) = %s[%22.15e]    [%22.15e]%s;\n",  i, j,name, (double)M[i +j * ldm], (double)M2[i +j * ldm2],name2);
+
+  return 0;
+}
