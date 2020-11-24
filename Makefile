Atakirk2/convgemm

commit 022f36502b6d1b47d6ced45c13e492ecad22ae12

BUGFIX: corrected a bug that made all convolution operations give an incorrect solution when h!=ho or w!=wo

convCommon.c
@@ -29,19 +29,21 @@
 void convolutionNaive(const int h, const int w, const int c,const int b,const float* In,const int kh,const int kw, const int kn, const float* F, float* Out, const int stride)
 {
 
-    int ic, ikh, ikw, ih, iw, ib, ik;    
+    int ic, ikh, ikw, ih, iw, ib, ik,ho,wo, pad =0; //padding currently unsuported    
     float ZERO = 0.0;
     
+    ho = floor((h - kh + 2 * pad) / stride + 1);
+    wo = floor((w - kw + 2 * pad) / stride + 1);
     
-    bli_ssetv(BLIS_NO_CONJUGATE,h*w*kn*b,&ZERO,Out,1);
+    bli_ssetv(BLIS_NO_CONJUGATE,ho*wo*kn*b,&ZERO,Out,1);
     for(ib = 0;ib < b; ib++)    
         for( ic = 0; ic < c; ic++)
-            for(iw = 0; iw < w; iw++)                
-                for(ih = 0; ih < h; ih++)
+            for(iw = 0; iw < wo; iw++)                
+                for(ih = 0; ih < ho; ih++)
                     for(ikw = 0; ikw < kw; ikw++)
                         for(ikh = 0; ikh < kh; ikh++)
                             for(ik=0;ik < kn; ik++)
-                                Out[ik + ( ib*w*h + iw*h + ih) *kn] += 
+                                Out[ik + ( ib*wo*ho + iw*ho + ih) *kn] += 
                                 In[ib * c*h*w + ic * h*w + (iw * stride + ikw) * h + (stride * ih + ikh)] 
                                 * F[ik + (ic * kh*kw + ikw * kh + ikh  ) * kn];
 
@@ -65,9 +67,10 @@ void convolutionNaive(const int h, const int w, const int c,const int b,const fl
 void im2Col(const int h, const int w, const int c, const int b,const float* In, const int kh, const int kw, const int stride,float* Out)
 {
     int ic, ikh, ikw, ih, iw, ib,
-        row,col;
+        row,col, ho,wo,pad =0; //padding currently unsuported
    
-    unsigned int cSize = h*w, //chanel memory leap
+    unsigned int cSize = h*w, //chanel memory leap in input tensor
+                 coSize = ho*wo, //chanel memory leap in output matix
                  kSize = kh*kw, //kernel memory leap (single chanel)
                  bSize = c*h*w, //batch memory leap
                  ckSize = c * kSize, //kernels memory leap (all channels)
@@ -81,22 +84,26 @@ void im2Col(const int h, const int w, const int c, const int b,const float* In, 
                  colib,
                  coliw;
     
+                 
+    ho = floor((h - kh + 2 * pad) / stride + 1);
+    wo = floor((w - kw + 2 * pad) / stride + 1);
+    
     for(ib = 0;ib < b; ib++)    
     {
-        colib = ib * cSize;
+        colib = ib * coSize;
         posib = ib * bSize;
         #pragma omp parallel for private (iw,ih,ikw,ikh,row,col,rowic,posic,coliw,posiw,posih,rowikw,posikw) 
         for( ic = 0; ic < c; ic++)
         {
             rowic = ic *kSize;
             posic = ic * cSize + posib;
-            for(iw = 0; iw < w; iw++)   
+            for(iw = 0; iw < wo; iw++)   
             {
-                coliw = colib + iw * h;
+                coliw = colib + iw * ho;
                 posiw = iw * stride * h + posic;
-                for(ih = 0; ih < h; ih++)
+                for(ih = 0; ih < ho; ih++)
                 {
-                     //OPT col = ib * cSize + iw * h + ih;
+                     //OPT col = ib * coSize + iw * h + ih;
                     col = coliw + ih;
                     posih = stride * ih;
                     for(ikw = 0; ikw < kw; ikw++)

gemmConv.c
@@ -1138,12 +1138,15 @@ void i8set0s_mxn(unsigned int m,unsigned int n,int8_t* restrict M,unsigned int l
  * @param[in] c number of chanels of input tensor.
  * @param[in] h input tensor hight.
  * @param[in] w input tensor width.
+ * @param[in] ho matrix B hight.
+ * @param[in] wo imatrix B width.
  * @param[in] kh kernel height.
  * @param[in] kw kernel width.
  * @param[in] stride Stride to apply the krnels to the input tensor.
  */
-void sPack_im2Col(unsigned int i, unsigned int j,float * restrict In, float * restrict B_pack, unsigned int k, unsigned int n,             
-                 unsigned int b, unsigned int c, unsigned int h, unsigned int w, 
+void sPack_im2Col(unsigned int i, unsigned int j,float * restrict In, float * restrict B_pack, unsigned int k, unsigned int n, unsigned int b, unsigned int c, 
+                 unsigned int h, unsigned int w, 
+                 unsigned int ho, unsigned int wo,
                  unsigned int kh, unsigned int kw, unsigned int stride)
 
 {
@@ -1152,13 +1155,15 @@ void sPack_im2Col(unsigned int i, unsigned int j,float * restrict In, float * re
                  pos, pos_ic, pos_ib, pos_ic_ikw; //position on the original image
     unsigned int pos_ic_ini,ikw_ini,ikh_ini,pos_ib_ini,iw_ini,ih_ini; //Initial values of indexes
     
-    unsigned int cSize = h*w, //chanel memory leap
+    unsigned int cSize = h*w, //chanel memory leap in input tensor
+                 coSize = ho*wo, //chanel memory leap in matrix B
                  kSize = kh*kw, //kernel memory leap (single chanel)
                  bSize = c*h*w; //batch memory leap
     
     unsigned int jc,pc,jr; //loop control indexes
 	float * restrict B_pack_local;
     unsigned int skipPos;
+    
     ic = i/kSize;
     ikw_ini = (i%kSize)/kh;
     ikh_ini = (i%kSize)%kh;
@@ -1174,9 +1179,9 @@ void sPack_im2Col(unsigned int i, unsigned int j,float * restrict In, float * re
         skipPos =BLOCK_NR - n_alg;
          
         j_local = j +jc;
-        ib = j_local/cSize;
-        iw_ini = (j_local%(cSize))/h;
-        ih_ini = (j_local%(cSize))%h;
+        ib = j_local/coSize;
+        iw_ini = (j_local%(coSize))/ho;
+        ih_ini = (j_local%(coSize))%ho;
         pos_ib_ini = ib * bSize;
 
         
@@ -1203,7 +1208,7 @@ void sPack_im2Col(unsigned int i, unsigned int j,float * restrict In, float * re
             pos_ib=pos_ib_ini;
             iw=iw_ini;
 			for(jr=0,ih=ih_ini;jr<n_alg;jr++,ih++){
-                if(ih==h)
+                if(ih==ho)
                 {
                     ih=0;
                     iw++;
@@ -1261,9 +1266,14 @@ void sgemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int k
 	float *Ar, *Br;
 	float *Cr;
 	float betaInner, zero =0.0;
+    
+    unsigned int ho, wo, pad = 0;//padding currently unsuported
+    
+    ho = floor((h - kh + 2 * pad) / stride + 1);
+    wo = floor((w - kw + 2 * pad) / stride + 1);
 
     unsigned int m = kn,
-                 n = h*w*b,
+                 n = ho*wo*b,
                  k = kh*kw*c;
           
     unsigned int lda= kn,
@@ -1284,7 +1294,7 @@ void sgemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int k
 			else
 				betaInner=beta;
 
-			sPack_im2Col(pc,jc, In, Bc_pack, k_alg, n_alg, b,c,h,w,kh,kw, stride);  //PACK B
+			sPack_im2Col(pc,jc, In, Bc_pack, k_alg, n_alg, b,c,h,w,ho,wo,kh,kw, stride);  //PACK B
 
 			for (unsigned int ic=0; ic<m; ic+=BLOCK_MC) {
 
@@ -1337,12 +1347,14 @@ void sgemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int k
  * @param[in] c number of chanels of input tensor.
  * @param[in] h input tensor hight.
  * @param[in] w input tensor width.
+ * @param[in] ho matrix B hight.
+ * @param[in] wo matrix B width.
  * @param[in] kh kernel height.
  * @param[in] kw kernel width.
  * @param[in] stride Stride to apply the krnels to the input tensor.
  */
-void hPack_im2Col(unsigned int i, unsigned int j,_Float16 * restrict In, _Float16 * restrict B_pack, unsigned int k, unsigned int n,             
-                 unsigned int b, unsigned int c, unsigned int h, unsigned int w, 
+void hPack_im2Col(unsigned int i, unsigned int j,_Float16 * restrict In, _Float16 * restrict B_pack, unsigned int k, unsigned int n, unsigned int b, unsigned int c, 
+                 unsigned int h, unsigned int w,  unsigned int ho, unsigned int wo,
                  unsigned int kh, unsigned int kw, unsigned int stride)
 
 {
@@ -1351,7 +1363,8 @@ void hPack_im2Col(unsigned int i, unsigned int j,_Float16 * restrict In, _Float1
                  pos, pos_ic, pos_ib, pos_ic_ikw; //position on the original image
     unsigned int pos_ic_ini,ikw_ini,ikh_ini,pos_ib_ini,iw_ini,ih_ini; //Initial values of indexes
     
-    unsigned int cSize = h*w, //chanel memory leap
+    unsigned int cSize = h*w, //chanel memory leap in input tensor
+                 coSize = ho*wo, //chanel memory leap in matrix B
                  kSize = kh*kw, //kernel memory leap (single chanel)
                  bSize = c*h*w; //batch memory leap
     
@@ -1374,9 +1387,9 @@ void hPack_im2Col(unsigned int i, unsigned int j,_Float16 * restrict In, _Float1
         skipPos =hBLOCK_NR - n_alg;
         
         j_local = j +jc;
-        ib = j_local/cSize;
-        iw_ini = (j_local%(cSize))/h;
-        ih_ini = (j_local%(cSize))%h;
+        ib = j_local/coSize;
+        iw_ini = (j_local%(coSize))/ho;
+        ih_ini = (j_local%(coSize))%ho;
         pos_ib_ini = ib * bSize;
 
         
@@ -1403,7 +1416,7 @@ void hPack_im2Col(unsigned int i, unsigned int j,_Float16 * restrict In, _Float1
             pos_ib=pos_ib_ini;
             iw=iw_ini;
 			for(jr=0,ih=ih_ini;jr<n_alg;jr++,ih++){
-                if(ih==h)
+                if(ih==ho)
                 {
                     ih=0;
                     iw++;
@@ -1462,8 +1475,13 @@ void hgemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int k
 	_Float16 *Cr;
 	_Float16 betaInner, zero =  0.0;
 
+    unsigned int ho, wo, pad = 0;//padding currently unsuported
+    
+    ho = floor((h - kh + 2 * pad) / stride + 1);
+    wo = floor((w - kw + 2 * pad) / stride + 1);
+    
     unsigned int m = kn,
-                 n = h*w*b,
+                 n = ho*wo*b,
                  k = kh*kw*c;
           
     unsigned int lda= kn,
@@ -1483,7 +1501,7 @@ void hgemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int k
 			else
 				betaInner=beta;
 
-			hPack_im2Col(pc,jc, In, Bc_pack, k_alg, n_alg, b,c,h,w,kh,kw, stride);  //PACK B
+			hPack_im2Col(pc,jc, In, Bc_pack, k_alg, n_alg, b,c,h,w,ho,wo,kh,kw, stride);  //PACK B
 
 			for (unsigned int ic=0; ic<m; ic+=hBLOCK_MC) {
 
@@ -1569,12 +1587,15 @@ void hgemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int k
  * @param[in] c number of chanels of input tensor.
  * @param[in] h input tensor hight.
  * @param[in] w input tensor width.
+ * @param[in] ho matrix B hight.
+ * @param[in] wo imatrix B width.
  * @param[in] kh kernel height.
  * @param[in] kw kernel width.
  * @param[in] stride Stride to apply the krnels to the input tensor.
  */
 void i16Pack_im2Col(unsigned int i, unsigned int j,int16_t * restrict In, int16_t * restrict B_pack, unsigned int k, unsigned int n,             
                  unsigned int b, unsigned int c, unsigned int h, unsigned int w, 
+                 unsigned int ho, unsigned int wo,
                  unsigned int kh, unsigned int kw, unsigned int stride)
 
 {
@@ -1583,7 +1604,8 @@ void i16Pack_im2Col(unsigned int i, unsigned int j,int16_t * restrict In, int16_
                  pos, pos_ic, pos_ib, pos_ic_ikw; //position on the original image
     unsigned int pos_ic_ini,ikw_ini,ikh_ini,pos_ib_ini,iw_ini,ih_ini; //Initial values of indexes
     
-    unsigned int cSize = h*w, //chanel memory leap
+    unsigned int cSize = h*w, //chanel memory leap in input tensor
+                 coSize = ho*wo, //chanel memory leap in matrix B
                  kSize = kh*kw, //kernel memory leap (single chanel)
                  bSize = c*h*w; //batch memory leap
     
@@ -1606,9 +1628,9 @@ void i16Pack_im2Col(unsigned int i, unsigned int j,int16_t * restrict In, int16_
         skipPos =hBLOCK_NR - n_alg;
         
         j_local = j +jc;
-        ib = j_local/cSize;
-        iw_ini = (j_local%(cSize))/h;
-        ih_ini = (j_local%(cSize))%h;
+        ib = j_local/coSize;
+        iw_ini = (j_local%(coSize))/ho;
+        ih_ini = (j_local%(coSize))%ho;
         pos_ib_ini = ib * bSize;
 
         
@@ -1635,7 +1657,7 @@ void i16Pack_im2Col(unsigned int i, unsigned int j,int16_t * restrict In, int16_
             pos_ib=pos_ib_ini;
             iw=iw_ini;
 			for(jr=0,ih=ih_ini;jr<n_alg;jr++,ih++){
-                if(ih==h)
+                if(ih==ho)
                 {
                     ih=0;
                     iw++;
@@ -1693,9 +1715,14 @@ void i16gemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int
 	int16_t *Ar, *Br;
 	int16_t *Cr;
 	int16_t betaInner, zero =  0.0;
+    
+    unsigned int ho, wo, pad = 0;//padding currently unsuported
 
+    ho = floor((h - kh + 2 * pad) / stride + 1);
+    wo = floor((w - kw + 2 * pad) / stride + 1);
+    
     unsigned int m = kn,
-                 n = h*w*b,
+                 n = ho*wo*b,
                  k = kh*kw*c;
           
     unsigned int lda= kn,
@@ -1715,7 +1742,7 @@ void i16gemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int
 			else
 				betaInner=beta;
 
-			i16Pack_im2Col(pc,jc, In, Bc_pack, k_alg, n_alg, b,c,h,w,kh,kw, stride);  //PACK B
+			i16Pack_im2Col(pc,jc, In, Bc_pack, k_alg, n_alg, b,c,h,w,ho,wo,kh,kw, stride);  //PACK B
 
 			for (unsigned int ic=0; ic<m; ic+=hBLOCK_MC) {
 

gemmConv.h
@@ -188,9 +188,9 @@ void sgemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int k
         float * Ac_pack, float * Bc_pack );
 
 //Packing routine
-void sPack_im2Col(unsigned int i, unsigned int j,float * restrict B, float * restrict B_pack, unsigned int k, unsigned int n,             
-                 unsigned int b, unsigned int c, unsigned int h, unsigned int w, 
-                 unsigned int kh, unsigned int kw, unsigned int stride);
+void sPack_im2Col(unsigned int i, unsigned int j,float * restrict B, float * restrict B_pack, 
+                  unsigned int k, unsigned int n, unsigned int b, unsigned int c, 
+                  unsigned int h, unsigned int w, unsigned int ho, unsigned int wo, unsigned int kh, unsigned int kw, unsigned int stride);
             
 
 /********half precision convolution gemm********/
@@ -202,9 +202,10 @@ void hgemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int k
         _Float16 * Ac_pack, _Float16 * Bc_pack );
 
 //Packing routine
-void hPack_im2Col(unsigned int i, unsigned int j,_Float16 * restrict In, _Float16 * restrict B_pack, unsigned int k, unsigned int n,             
-                 unsigned int b, unsigned int c, unsigned int h, unsigned int w, 
-                 unsigned int kh, unsigned int kw, unsigned int stride);
+void hPack_im2Col(unsigned int i, unsigned int j,_Float16 * restrict In, _Float16 * restrict B_pack,        
+                unsigned int k, unsigned int n, unsigned int b, unsigned int c, 
+                unsigned int h, unsigned int w, unsigned int ho, unsigned int wo,
+                unsigned int kh, unsigned int kw, unsigned int stride);
 
 /***** Int 16 convolution****/
 void i16gemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int kn,
@@ -215,6 +216,7 @@ void i16gemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int
         int16_t * Ac_pack, int16_t * Bc_pack );
 
 //Packing routine
-void i16Pack_im2Col(unsigned int i, unsigned int j,int16_t * restrict In, int16_t * restrict B_pack, unsigned int k, unsigned int n,             
-                 unsigned int b, unsigned int c, unsigned int h, unsigned int w, 
-                 unsigned int kh, unsigned int kw, unsigned int stride);
+void i16Pack_im2Col(unsigned int i, unsigned int j,int16_t * restrict In, int16_t * restrict B_pack, 
+                unsigned int k, unsigned int n, unsigned int b, unsigned int c, 
+                unsigned int h, unsigned int w, unsigned int ho, unsigned int wo,  
+                unsigned int kh, unsigned int kw, unsigned int stride);

testIm2Col.c
@@ -111,12 +111,12 @@ int main( int argc, char** argv )
     {
         //Timing naive convolution
         tIni = bli_clock();
-        convolutionNaive(ho,wo,c,b,In,kh,kw,kn,F,OutConv, stride);
+        convolutionNaive(h,w,c,b,In,kh,kw,kn,F,OutConv, stride);
         tConv += bli_clock() - tIni;
 
         //Timing im2col +gemm
         tIni = bli_clock();
-        im2Col (ho,wo,c,b,In,kh,kw, stride,Aux);
+        im2Col (h,w,c,b,In,kh,kw, stride,Aux);
         tIm2Col += bli_clock() -tIni;
         bli_sgemm(BLIS_NO_TRANSPOSE,BLIS_NO_TRANSPOSE,kn,ho*wo*b,kh*kw*c,&ONE,F,1,kn,Aux,1,kh*kw*c,&ZERO,OutI2c,1,kn);
         //sgemm_cust(kn,ho*wo*b,kh*kw*c,1,F,kn,Aux,kh*kw*c,0,OutI2c,kn,Ac_pack,Bc_pack);
@@ -126,7 +126,7 @@ int main( int argc, char** argv )
         
         //Timing implicint gemm
         tIni = bli_clock();
-        sgemm_conv(kh,kw,c,kn,1,F, ho,wo,b, stride, In, 0,OutImp,Ac_pack,Bc_pack);
+        sgemm_conv(kh,kw,c,kn,1,F, h,w,b, stride, In, 0,OutImp,Ac_pack,Bc_pack);
         tImp += bli_clock() -tIni;
 
     }

commit 3f3b9aa1c5c19034164e6068691408bc02cf443d

BUGFIX: corrected bug introduced in commit 022f36502b6d1b47d6ced45c13e492ecad22ae12 BUGFIX: corrcted a paralelism bug introduced in the elimination of reference microkernel Improved compilation for external users

Makefile
@@ -5,7 +5,7 @@ CFLAGS= -Wl,-rpath,$(LIBPATH) -L$(LIBPATH) -fpic -fopenmp $(COPTFLAGS) $(OPTS)
 ARCH=A57
 ifeq ($(ARCH),Carmel)
 	ARCHFLAGS= -mtune=cortex-a76  -march=armv8.2-a+fp16fml -Dfp16_support #NVIDIA Carmel
-else ifeq($(ARCH),A57)
+else ifeq ($(ARCH),A57)
 	ARCHFLAGS= -mtune=cortex-a57 -march=armv8-a+fp+simd -mcpu=cortex-a57 #Cortex A-57
 else ifeq ($(ARCH),A72) # Raspberry Pi 4
 	ARCHFLAGS= -mtune=cortex-a72 -march=armv8-a+fp+crc -mcpu=cortex-a72 
@@ -30,7 +30,7 @@ eval: convEval.x
 micro: testMicrokernels.x
 blocks: evalBlockSize.x
 peakPerf: peakPerfTest.x
-lib: $(LIBPATH)libgemmConv.so
+lib: $(LIBPATH)/libgemmConv.so
 
 %.o: %.c 
 	$(CC) -c -o $@ $< $(CFLAGS) -I$(INCLUDE)
@@ -38,8 +38,8 @@ lib: $(LIBPATH)libgemmConv.so
 gemmConv.o: gemmConv.c $(uKOBJS) gemmConv.h
 	$(CC) -c -o $@ $< $(CFLAGS) -I$(INCLUDE)
 
-$(LIBPATH)libgemmConv.so: gemmConv.o 
-	$(CC) -shared -o $@  $< $(uKOBJS)
+$(LIBPATH)/libgemmConv.so: gemmConv.o 
+	$(CC) -shared -fopenmp -o $@  $< $(uKOBJS)
 	
 convCommon.o: convCommon.c convCommon.h
 	$(CC) -c -o $@ $< $(CFLAGS) -I$(INCLUDE)
@@ -51,7 +51,7 @@ testMicrokernels.x: testMicrokernels.o gemmConv.o
 	$(CC) -o $@ $^ $(uKOBJS) $(CFLAGS) $(LIB)	
 
 
-%.x: %.o $(LIBPATH)libgemmConv.so convCommon.o 
+%.x: %.o $(LIBPATH)/libgemmConv.so convCommon.o 
 	$(CC) -o $@ $^  $(CFLAGS) $(LIB)
 
 clean:

convCommon.c
@@ -69,6 +69,9 @@ void im2Col(const int h, const int w, const int c, const int b,const float* In, 
     int ic, ikh, ikw, ih, iw, ib,
         row,col, ho,wo,pad =0; //padding currently unsuported
    
+    ho = floor((h - kh + 2 * pad) / stride + 1);
+    wo = floor((w - kw + 2 * pad) / stride + 1);
+   
     unsigned int cSize = h*w, //chanel memory leap in input tensor
                  coSize = ho*wo, //chanel memory leap in output matix
                  kSize = kh*kw, //kernel memory leap (single chanel)
@@ -85,8 +88,7 @@ void im2Col(const int h, const int w, const int c, const int b,const float* In, 
                  coliw;
     
                  
-    ho = floor((h - kh + 2 * pad) / stride + 1);
-    wo = floor((w - kw + 2 * pad) / stride + 1);
+
     
     for(ib = 0;ib < b; ib++)    
     {
@@ -103,7 +105,7 @@ void im2Col(const int h, const int w, const int c, const int b,const float* In, 
                 posiw = iw * stride * h + posic;
                 for(ih = 0; ih < ho; ih++)
                 {
-                     //OPT col = ib * coSize + iw * h + ih;
+                     //OPT col = ib * coSize + iw * ho + ih;
                     col = coliw + ih;
                     posih = stride * ih;
                     for(ikw = 0; ikw < kw; ikw++)

gemmConv.c
@@ -1212,7 +1212,7 @@ void sPack_im2Col(unsigned int i, unsigned int j,float * restrict In, float * re
                 {
                     ih=0;
                     iw++;
-                    if(iw==w)
+                    if(iw==wo)
                     {
                         iw=0;
                         pos_ib += bSize;//OPT ib++;pos_in = ib*bSize;
@@ -1307,7 +1307,7 @@ void sgemm_conv(unsigned int kh, unsigned int kw, unsigned int c, unsigned int k
 				Cc=&C[ic+jc*ldc];
 
 
-                #pragma omp parallel for  private(Ar, Br, Cr) 
+                #pragma omp parallel for  private(Ar, Br, Cr,CBuff) 
 				for(unsigned jr=0;jr<n_alg;jr+=BLOCK_NR){
 					unsigned int nr_alg=fmin(BLOCK_NR,n_alg-jr);
 					for(unsigned int ir=0;ir<m_alg;ir+=BLOCK_MR){